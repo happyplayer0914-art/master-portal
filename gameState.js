24e80d7a476910c74de8a084b37db5b4a58bdb12
happyplayer0914-art <happyplayer0914@gmail.com>
2026-03-11 03:46:16 +0900

    Update gameState.js

--- gameState.js
+++ gameState.js
@@ -5,7 +5,7 @@ const GameState = {
     nickname: "위대한 길드장",
     deviceId: "", // 🔥 기기 식별자
     gold: 10000, gem: 10000, lastCheckIn: "",
-    lastIdleCheck: Date.now(), // 🔥 방치형 보상 체크 시간 추가
+    lastIdleCheck: Date.now(),
     rpgStage: 1, rpgAtk: 10, rpgMaxHp: 100, currentHp: 100,
     potions: 1, inventory: [], equippedGear: null, equippedSkin: null,
     isBattling: false,
@@ -23,7 +23,7 @@ const GameState = {
         this.gold = parseInt(localStorage.getItem('master_gold') || "10000");
         this.gem = parseInt(localStorage.getItem('master_gem') || "10000");
         this.lastCheckIn = localStorage.getItem('last_checkin') || "";
-        this.lastIdleCheck = parseInt(localStorage.getItem('master_last_idle') || Date.now()); // 불러오기
+        this.lastIdleCheck = parseInt(localStorage.getItem('master_last_idle') || Date.now());
         this.rpgStage = parseInt(localStorage.getItem('master_stage') || "1");
         this.rpgAtk = parseInt(localStorage.getItem('master_atk') || "10");
         this.rpgMaxHp = parseInt(localStorage.getItem('master_max_hp') || "100");
@@ -34,18 +34,16 @@ const GameState = {
         this.equippedSkin = localStorage.getItem('master_equipped_skin');
 
         this.checkAndRevive();
-        if (localStorage.getItem('master_in_battle') === 'true') {
-            localStorage.removeItem('master_in_battle');
-            this.isBattling = false;
-            setTimeout(() => alert(`🚨 전투 도중 이탈하여 처음 위치로 돌아왔습니다.`), 500);
-        }
+        
+        // 🔥 Bug #2 수정: load 시점의 직접적인 UI 호출(alert)을 제거하고 
+        // UIManager.init()에서 처리하도록 master_in_battle 상태는 유지합니다.
     },
 
     save() {
         localStorage.setItem('master_nickname', this.nickname);
         localStorage.setItem('master_gold', this.gold);
         localStorage.setItem('master_gem', this.gem);
-        localStorage.setItem('master_last_idle', this.lastIdleCheck); // 저장하기
+        localStorage.setItem('master_last_idle', this.lastIdleCheck);
         localStorage.setItem('master_stage', this.rpgStage);
         localStorage.setItem('master_atk', this.rpgAtk);
         localStorage.setItem('master_max_hp', this.rpgMaxHp);
@@ -69,5 +67,4 @@ const GameState = {
         if(this.equippedGear && GameData.items[this.equippedGear]) mult = GameData.items[this.equippedGear].statMult;
         return { atk: Math.floor(this.rpgAtk * mult), hp: Math.floor(this.rpgMaxHp * mult) };
     }
-};;
-
+};
