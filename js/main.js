234bb795634f99c99589765eb6b7b258f6502473
happyplayer0914-art <happyplayer0914@gmail.com>
2026-03-13 14:36:29 +0900

    Update main.js

--- js/main.js
+++ js/main.js
@@ -7,9 +7,12 @@ window.onload = function() {
     
     try {
         GameState.load();
+        
+        // 🌟 [여기에 추가!!] 게임 켜질 때 퀘스트 진행도 싹 불러오기!
+        if (GameSystem.Quest) GameSystem.Quest.init(); 
+
         // 💡 [신규 추가] 게임 켜질 때 닉네임 고정 버튼 숨길지 말지 결정!
         GameSystem.applyNicknameUI();
         UIManager.init();
     } catch(e) { console.error("초기화 오류:", e); }
-};;
-
+};
