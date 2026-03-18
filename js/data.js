7626d67d3af65dc541caeacc1d85b499d54a8157
happyplayer0914-art <happyplayer0914@gmail.com>
2026-03-18 22:09:45 +0900

    Update data.js

--- js/data.js
+++ js/data.js
@@ -655,7 +655,7 @@ const GameData = {
             { id: 'bg_abyss', name: '심연의 옥좌', desc: '마왕이 머물던 서늘한 공간', price: 300, type: 'bg', img: 'bg_abyss.png' },
             { id: 'bg_ella', name: '엘라 테스트 배경', desc: '엘라 테스트 공간', price: 300, type: 'bg', img: 'bg_ella.png' },
             { id: 'bg_entj', name: 'entj 테스트 배경', desc: 'entj 테스트 공간', price: 300, type: 'bg', img: 'bg_entj.png' },
-            { id: 'bg_entj', name: '심연의 비밀 연구소', desc: '대마법사 멜리나의 연구실', price: 1500, type: 'bg', img: 'bg_intj.png' }
+            { id: 'bg_intj', name: '심연의 비밀 연구소', desc: '대마법사 멜리나의 연구실', price: 1500, type: 'bg', img: 'bg_intj.png' }
         ],
         bubbles: [
             { id: 'bb_gold', name: '황금빛 외침', desc: '채팅창의 시선을 독점하세요', price: 150, type: 'bubble', bgClass: 'bg-gradient-to-r from-yellow-600 to-yellow-400 text-black font-bold' }
